3c4e4a301705b88ce4c69b90f0f52a309551eda3
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-07-09 11:04:06 +1000

    added try catch to register user action

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -1,4 +1,4 @@
-import { AUTH_TOKEN } from "./../actions/types";
+import { AUTH_TOKEN, ERROR } from "./../actions/types";
 
 const defaultState = {
   token: sessionStorage.getItem("token") || null
@@ -8,6 +8,8 @@ export default (state = defaultState, action) => {
   switch (action.type) {
     case AUTH_TOKEN:
       return { ...state, token: action.payload };
+    case ERROR:
+      return { ...state, error: action.payload };
     default:
       return state;
   }
