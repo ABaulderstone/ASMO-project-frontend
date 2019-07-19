262a826c0be45acfd92ff58acd74f8a5940de651
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-19 11:36:00 +1000

    comments page list working

--- src/components/pages/CommentsShowPage.js
+++ src/components/pages/CommentsShowPage.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
 import Navbar from "./../navbar/Navbar";
+import { connect } from "react-redux"
+import {fetchComments} from "./../../actions/index"
+
 // import { Link } from "react-router-dom";
 
 class CommentsShowPage extends Component {
+    componentDidMount() {
+        this.props.fetchComments();
+    }
+
     render() {
+        const {reviews} = this.props;
         return (
             <>
                 <Navbar />    
                     <div>
                         <h1>Comments</h1>
+
+                        <ul> {reviews.map (review => {
+                            return(<li key={review._id}>
+                                {review.comment}
+                            </li>)
+                        })}
+
+                        </ul>
+                        
                     </div>
             </>
         );
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        reviews: state.review.reviews
+    }
+}
 
-export default CommentsShowPage;
+export default connect(mapStateToProps, {fetchComments})(CommentsShowPage);
