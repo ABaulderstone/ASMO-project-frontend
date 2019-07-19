640f6f2425ed023e0f2f39465c2a9cd286aa576e
Sascha Kim <saschakim@Saschas-MacBook-Pro.local>
2019-07-19 14:41:20 +1000

    created edit staff component and page

---
+++ src/components/pages/EditStaffPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import EditStaff from "./../staff/EditStaff";
+
+class EditStaffPage extends Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default EditStaffPage;
