b5d7becd55a5a50717c2020d90cbea16774c0521
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-20 10:38:39 +1000

    massive refactor of api calls making app a lot easier to understand

--- src/components/pages/ReviewPage.js
+++ src/components/pages/ReviewPage.js
@@ -1,31 +1,20 @@
 import React, { Component } from "react";
 import ReviewForm from "./../forms/ReviewForm";
-import { connect } from "react-redux";
-import { resetReview } from "../../actions";
+
 
 
 
 class ReviewPage extends Component {
     render() {
-        const {error} = this.props;
-    if (this.props.review.isSubmitted) {
-        this.props.resetReview();
-        this.props.history.push("/thankyou")
-    }
         return(
             <div className="ui container">
                 <div className="ui segment">
-                    {error && <div> {error} </div>}
                     <ReviewForm {...this.props} />
                 </div>
             </div>
         );
     }
 }
-function mapStateToProps(state) {
-    return {
-      review: state.review
-    }
-  }
 
-export default connect(mapStateToProps, {resetReview})(ReviewPage);
+
+export default ReviewPage;
