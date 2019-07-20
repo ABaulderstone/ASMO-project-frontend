a6b29b32ce1183e71bd5336de93102857f79ae55
Sascha Kim <saschakim@Saschas-MBP.home.gateway>
2019-07-20 15:31:38 +1000

    linked edit button to CustomerEditPage

--- src/components/pages/CustomerShowPage.js
+++ src/components/pages/CustomerShowPage.js
@@ -2,52 +2,54 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./../navbar/Navbar";
 import { fetchCustomers } from "./../../actions/index";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import LocalAPI from "./../../apis/local";
 import CustomerItem from "./../../components/CustomerItem";
 
 class CustomerShowPage extends Component {
-    state = {
-        customers: []
-    }
-    componentDidMount() {
-      LocalAPI.get("/customers")
-        .then(response => {
-            this.setState({
-                customers: response.data
-            })
-        })
-    }
+  state = {
+    customers: []
+  };
+  componentDidMount() {
+    LocalAPI.get("/customers").then(response => {
+      this.setState({
+        customers: response.data
+      });
+    });
+  }
 
-    
-   
-    render() {
-         const { customers } = this.state;
-        console.log(customers)
-        return (
-            <>
-            <Navbar />
-                <h1>Customers</h1>
-                <div className="ui container">
-                
-                <table className="ui celled table">
-                <thead>
-                    <tr><th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Edit</th>
-                    </tr>
-                </thead>
-                {customers.map(customer => {
-                    return <CustomerItem id={customer.id} name={customer.name} email={customer.email} phone={customer.phone} />
-                })}
-                </table>
-                
-                </div>
-            </>
-        );
-    }
+  render() {
+    const { customers } = this.state;
+    console.log(customers);
+    return (
+      <>
+        <Navbar />
+        <h1>Customers</h1>
+        <div className="ui container">
+          <table className="ui celled table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Edit</th>
+              </tr>
+            </thead>
+            {customers.map(customer => {
+              return (
+                <CustomerItem
+                  id={customer._id}
+                  name={customer.name}
+                  email={customer.email}
+                  phone={customer.phone}
+                />
+              );
+            })}
+          </table>
+        </div>
+      </>
+    );
+  }
 }
 
-
-export default CustomerShowPage
+export default CustomerShowPage;
