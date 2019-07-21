2a98cedbbd3bf55cd2a056705e93863c228d673f
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-21 13:19:14 +1000

    added customer card to show customer on search page

---
+++ src/components/CustomerCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CustomerCard = props => {
+  return (
+    <div className="ui card" id={props.id}>
+      <div className="content">
+      <div className="header">{props.name}</div>
+      <div className= "description">{props.address}</div>
+      </div>
+      <div className="extra content">
+        Email:{props.email} Phone Number: {props.phone}
+      </div>
+    </div>
+  );
+};
+
+export default CustomerCard;
