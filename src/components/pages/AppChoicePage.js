af77e30e3d00f987fa86c00da9837204d7c9c8b9
Sascha Kim <saschakim@Saschas-MacBook-Pro.local>
2019-07-22 12:45:34 +1000

    merged with dev

--- src/components/pages/AppChoicePage.js
+++ src/components/pages/AppChoicePage.js
@@ -2,30 +2,28 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./../navbar/Navbar";
 
-
 class AppChoicePage extends Component {
-    render() {
-        return (
-            <>
-                <Navbar />
-                    <div>
-                        <h1>Choose</h1>
-                        <div>
-                            <Link to="/review" target="_blank">
-                                <button>Survey</button>
-                            </Link>
-                            <Link to="/customer" target="_blank">
-                                <button>Member</button>
-                            </Link>
-                            <Link to="/member_search" target="_blank">
-                                <button>Search</button>
-                            </Link>
-                        
-                        </div>
-                    </div>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div>
+          <h1>Choose</h1>
+          <div>
+            <Link to="/review" target="_blank">
+              <button>Survey</button>
+            </Link>
+            <Link to="/customer" target="_blank">
+              <button>Member</button>
+            </Link>
+            <Link to="/member_search" target="_blank">
+              <button>Search</button>
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default AppChoicePage;
