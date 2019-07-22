e8f2b9fc47be319df64679105aaba0ae24aac3e0
sjwconway <sjwconway@hotmail.com>
2019-07-22 16:24:44 +1000

    added styling to member_search page

--- src/components/CustomerCard.js
+++ src/components/CustomerCard.js
@@ -2,13 +2,16 @@ import React from "react";
 
 const CustomerCard = props => {
   return (
-    <div className="ui card" id={props.id}>
+    <div className="ui card" id={props.id} style={{width: "80%", textAlign: "center", margin: "auto"}}>
       <div className="content">
       <div className="header">{props.name}</div>
       <div className= "description">{props.address || "No listed address"}</div>
       </div>
       <div className="extra content">
-        Email:{props.email} Phone Number: {props.phone}
+        <div>Email:{props.email}</div> 
+        <div>Phone Number: {props.phone}</div>
+        <div>Birthday: {props.dob || "No birthday given"}</div>
+        <div>Anniversary: {props.anniversary || "No anniversary given"}</div>
       </div>
     </div>
   );
