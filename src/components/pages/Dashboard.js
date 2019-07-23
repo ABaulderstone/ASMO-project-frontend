ad6d31606d2ff00cb75411e780d1ab3d9ad78e23
Sascha Kim <saschakim@Saschas-MacBook-Pro.local>
2019-07-23 14:42:22 +1000

    added chart to dashboard

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,24 +1,53 @@
 import React, { Component } from "react";
 import Navbar from "./../navbar/Navbar";
-import 'bulma/css/bulma.css';
-import BarChart from "../chart/BarChart"
+import "bulma/css/bulma.css";
+import BarChart from "./../chart/BarChart";
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <>
-                <Navbar />
-                <div>
-                    <h1>Dashboard</h1>
-                    <div>
-                        Land of Smiles
-                </div>
+  constructor() {
+    super();
+    this.state = {
+      chartData: {}
+    };
+  }
 
-                    <BarChart />
-                </div>
-            </>
-        );
-    }
+  componentWillMount() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    // ajax calls here
+    this.setState({
+      chartData: {
+        labels: ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"],
+        datasets: [
+          {
+            label: "Kitchen",
+            data: [1, 2, 5, 4],
+            backgroundColor: "#D6E9C6"
+          },
+          {
+            label: "Floor",
+            data: [1, 2, 5, 4],
+            backgroundColor: "#EBCCD1"
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div>
+          <h1>Dashboard</h1>
+          <div>Land of Smiles</div>
+          <BarChart chartData={this.state.chartData} legendPosition="bottom" />
+        </div>
+      </>
+    );
+  }
 }
 
 export default Dashboard;
