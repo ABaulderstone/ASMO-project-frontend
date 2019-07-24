23f4082127c81a25d909d870f29fdc9738f9ab46
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-25 00:27:57 +1000

    fixed radio button issue

--- src/components/forms/fields/Input.js
+++ src/components/forms/fields/Input.js
@@ -3,7 +3,7 @@ import React from "react";
 const Input = (props) => {
  return (
      <>
-     <input {...props.input} type={props.type} placeholder={props.placeholder} value={props.value} /> 
+     <input {...props.input} type={props.type} placeholder={props.placeholder} /> 
      <span>{props.meta.touched && props.meta.error}</span>
      </>
  )
