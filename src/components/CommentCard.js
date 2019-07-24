632bdfdef046b8c41cc5a5b57fdc5a04479ac0c5
Alex Baulderstone <alex.baulderstone@gmail.com>
2019-07-25 01:00:22 +1000

    formatted date on comment card

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,10 +1,17 @@
 import React from "react";
+import moment from "moment"
+import "moment-timezone";
 
 const CommentCard = props => {
+    const formattedDate = moment(props.date)
+    .tz("Australia/Sydney")
+    .format("DD-MM-YYYY HH:mm");
+    console.log(formattedDate);
+
   return (
     
     <div className="ui card" key={props.id} style={{width: "100%"}}>
-    <div className="content"><div className="header">{props.date}</div></div>
+    <div className="content"><div className="header">{formattedDate}</div></div>
       <div style={{fontSize: "1.8vh"}} className="content">{props.comment}</div>
       <div className="extra content">
         Food: {props.foodRating} Service: {props.serviceRating}
